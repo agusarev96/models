e9dc9fe5169511083c1c9ed9e7821dfb1ed65c47
agusarev <agusarev96@gmail.com>
2025-11-10 17:36:18 +0300

    create DataSet type for model

---
+++ data_utils/dataset.py
@@ -0,0 +1,37 @@
+from typing import List, Union
+
+import pandas as pd
+import numpy as np
+
+
+class DataSet:
+    
+    def __init__(self, data:Union[pd.DataFrame, np.ndarray], feature_names:List=[]):
+        if (type(data) == np.ndarray) and (len(feature_names) == 0):
+            raise ValueError("If you pass numpy array you need to specify `feature_names`")
+        
+        if (type(data) == np.ndarray) and (len(data.shape) != 2):
+            raise ValueError("Data need to be matrix M*N")
+        
+        if len(feature_names) != 0:
+            if type(data) == pd.DataFrame:
+                if len(feature_names) != len(data.columns):
+                    raise ValueError("length of feature_names doesn't match length of df columns")
+            else:
+                if len(feature_names) != data.shape[1]:
+                    raise ValueError("length of feature_names doesn't match length of data.shape[1]")
+        
+        self.feature_names = feature_names if feature_names else list(data.columns)
+        self.data = data.values if type(data) == pd.DataFrame else data
+        
+    def __str__(self) -> str:
+        return (
+            f"columns: {self.feature_names}\n"
+            f"shape: {self.data.shape}"
+        )
+        
+    def __repr__(self) -> str:
+        return (
+            f"columns: {self.feature_names}\n"
+            f"shape: {self.data.shape}"
+        )
